1495f1080bd73f438e95c118a28b845f1978756d
EC2 Default User <ec2-user@ip-172-31-13-63.ec2.internal>
2021-05-08 06:56:22 +0000

    create admin with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    root to: 'homes#top'
+    # get 'about' => 'homes#about'
+    
+    
+    
+  end
 end
